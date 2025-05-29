152f56b4da987dfdbb7fc4adbb3b12df5a84c5df
Martin Mohns <mamoh55@gmail.com>
2025-05-29 16:48:28 +0200

    Added Infrastructure folder with swagger config

---
+++ ApiGateway/Infrastructure/Documentation/SwaggerConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using MMLib.SwaggerForOcelot.Configuration;
+
+namespace ApiGateway.Infrastructure.Documentation;
+
+public static class SwaggerConfiguration
+{
+    public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Add endpoints API explorer
+        services.AddEndpointsApiExplorer();
+
+        // Configure and add SwaggerForOcelot
+        services.AddSwaggerForOcelot(configuration);
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app, IConfiguration configuration)
+    {
+        // Use standard Swagger middleware
+        app.UseSwagger();
+
+        // Configure Swagger for Ocelot with unified endpoint
+        app.UseSwaggerForOcelotUI(options =>
+        {
+            options.PathToSwaggerGenerator = "/swagger/docs";
+        });
+
+        return app;
+    }
+}
